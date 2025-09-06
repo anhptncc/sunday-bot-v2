c7393b5cf8c55e7d2d720cdd2d36dcb68f2bf6f1
anhpt <anh.phamtien@ncc.asia>
2025-09-06 11:57:05 +0700

    update countdown migration

--- src/migrations/1755683264327-CreateCountdownTable.ts
+++ src/migrations/1755683264327-CreateCountdownTable.ts
@@ -5,6 +5,10 @@ export class CreateCountdownTable1755683264327 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "countdown" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, "type" character varying NOT NULL, "targetDate" date, "isFinished" boolean NOT NULL DEFAULT false, CONSTRAINT "PK_f7cf818eea441890243008e506c" PRIMARY KEY ("id"))`);
+            // Insert sample countdown entry
+        await queryRunner.query(
+        `INSERT INTO "countdown" ("type", "targetDate", "isFinished") VALUES ('payday', NULL, false)`,
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
